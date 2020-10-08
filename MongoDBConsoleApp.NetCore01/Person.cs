2f7289230cdb30bf687983fe5a6386c395db46d0
Aydın Türker <doktoralban@outlook.com>
2020-10-08 18:19:22 +0300

    Add project files.

---
+++ MongoDBConsoleApp.NetCore01/Person.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Entities;
+using System;
+
+namespace MongoDBConsoleApp.NetCore01
+{
+    internal class Person : Entity
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int SiblingCount { get; set; }
+    }
+}
---
+++ MongoDBConsoleApp.NetCore01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Entities;
+using System.Threading.Tasks;
+
+namespace MongoDBConsoleApp.NetCore01
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await DB.InitAsync("MyDatabase", "localhost", 27017);
+
+            var prs = new Person
+            {
+                Name = "Aydın Türker",
+                DateOfBirth = new DateTime(1980, 10, 07),
+                SiblingCount =3
+            };
+
+            await prs.SaveAsync();
+
+            Console.WriteLine($"Person's ID: {prs.ID}");
+            Console.Read();
+        }
+
+
+
+    }
+}
